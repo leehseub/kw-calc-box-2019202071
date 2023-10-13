6f71854a536f8081a788bba6c60d7b70157ab2dc
leehseub <leehseub@naver.com>
2023-10-13 11:05:01 +0900

    2023/10/13 11:05

---
+++ lib.js
@@ -0,0 +1,22 @@
+function avg(numbers)
+{
+    let s=numbbers.reduce((prev,curr)=>prev+curr,0);
+    return s/numbers.length;
+}
+
+function prime(num)
+{
+    return false;
+}
+
+function fact(num)
+{
+    return -1;
+}
+
+module.export = 
+{
+    avg,
+    prime,
+    fact
+}
