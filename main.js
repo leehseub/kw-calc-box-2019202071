681089fddb67459fff53cf2c53a512774866d786
leehseub <leehseub@naver.com>
2023-10-27 20:47:26 +0900

    Complete

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const lib = require("./lib");
 
+if (process.argv.length <= 3) {
+    console.log("Insufficient parameter!");
+    process.exit(1);
+}
+
 let command = process.argv[2];
 
 let numbers=process.argv.slice(3,process.argv.length).map((n)=>parseFloat(n));
